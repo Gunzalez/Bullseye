c1dabb820dba5ec36301806d3d4b26927490146f
gunzalez <gunzalez@hotmail.com>
2021-05-23 21:46:15 +0100

    Initial commit

--- bullseye/ContentView.swift
+++ bullseye/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var sliderValue: Double = 60
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack{
+                Text("🎯🎯🎯\nPut the bullseye as close as you can to")
+                    .kerning(2.0)
+                    .bold()
+                    .font(.footnote)
+                    .lineSpacing(4.0)
+                    .multilineTextAlignment(.center)
+                Text("89")
+                HStack
+                {
+                    Text("1")
+                    Slider(value: $sliderValue, in: 1...100)
+                    Text("100")
+                }
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Hit Me!")
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
     }
 }
