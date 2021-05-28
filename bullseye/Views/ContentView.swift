c36e38920be85fea133a92931b94af34d22ec912
gunzalez <gunzalez@hotmail.com>
2021-05-28 23:11:43 +0100

    Add colour sets

--- bullseye/Views/ContentView.swift
+++ bullseye/Views/ContentView.swift
@@ -15,48 +15,52 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            VStack{
-                Text("🎯🎯🎯\nPut the bullseye as close as you can to")
-                    .kerning(2.0)
-                    .bold()
-                    .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
-                    .font(.footnote)
-                    .lineSpacing(4.0)
-                    .multilineTextAlignment(.center)
-                Text(String(game.target))
-                    .kerning(-1.0)
-                    .fontWeight(.black)
-                    .font(.largeTitle)
-                HStack{
-                    Text("1").bold().font(/*@START_MENU_TOKEN@*/.callout/*@END_MENU_TOKEN@*/)
-                    Slider(value: $silderValue, in: 1...100)
-                    Text("100").bold().font(/*@START_MENU_TOKEN@*/.callout/*@END_MENU_TOKEN@*/)
-                }
-                .padding()
-                Button(action: {
-                    alertIsVisible = true
-                }) {
-                    Text("Hit Me")
+            ZStack {
+                Color("Background")
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                VStack{
+                    Text("🎯🎯🎯\nPut the bullseye as close as you can to")
+                        .kerning(2.0)
                         .bold()
                         .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
-                        .font(.title3)
-                        .padding(20)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(21)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 21)
-                                .stroke(Color.white, lineWidth: 5)
-                        )
+                        .font(.footnote)
+                        .lineSpacing(4.0)
+                        .multilineTextAlignment(.center)
+                    Text(String(game.target))
+                        .kerning(-1.0)
+                        .fontWeight(.black)
+                        .font(.largeTitle)
+                    HStack{
+                        Text("1").bold().font(/*@START_MENU_TOKEN@*/.callout/*@END_MENU_TOKEN@*/)
+                        Slider(value: $silderValue, in: 1...100)
+                        Text("100").bold().font(/*@START_MENU_TOKEN@*/.callout/*@END_MENU_TOKEN@*/)
+                    }
+                    .padding()
+                    Button(action: {
+                        alertIsVisible = true
+                    }) {
+                        Text("Hit Me")
+                            .bold()
+                            .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                            .font(.title3)
+                            .padding(20)
+                            .background(Color("Button"))
+                            .foregroundColor(.white)
+                            .cornerRadius(21)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 21)
+                                    .stroke(Color(.white), lineWidth: 4)
+                            )
+                    }
+                    .alert(isPresented: $alertIsVisible, content: {
+                        
+                        let roundedValue: Int = Int(silderValue.rounded())
+                        
+                        return Alert(title: Text("Bullseye!"),
+                                     message: Text("Your slider is at \(roundedValue).\n You score \(game.points(sliderValue: roundedValue)) points thisd round."),
+                                     dismissButton: .default(Text("Awesome!")))
+                    })
                 }
-                .alert(isPresented: $alertIsVisible, content: {
-                    
-                    let roundedValue: Int = Int(silderValue.rounded())
-                    
-                    return Alert(title: Text("Bullseye!"),
-                                 message: Text("Your slider is at \(roundedValue).\n You score \(game.points(sliderValue: roundedValue)) points thisd round."),
-                                 dismissButton: .default(Text("Awesome!")))
-                })
             }
         }
     }
