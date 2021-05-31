d3feb408fdde286dbe91fa0469201e2b4261d476
gunzalez <gunzalez@hotmail.com>
2021-05-31 10:42:16 +0100

    Add newRound function and tests

--- BullseyeTests/BullseyeTests.swift
+++ BullseyeTests/BullseyeTests.swift
@@ -34,5 +34,11 @@ class BullseyeTests: XCTestCase {
         let score = game.points(sliderValue: guess)
         XCTAssertEqual(score, 95)
     }
+    
+    func testNewRound(){
+        game.startNewRound(points: 100)
+        XCTAssertEqual(game.score, 100)
+        XCTAssertEqual(game.round, 2)
+    }
 
 }
--- bullseye/Model/Game.swift
+++ bullseye/Model/Game.swift
@@ -9,12 +9,19 @@ import Foundation
 
 struct Game {
     var target = Int.random(in: 1...100)
-    var score = 999
-    var round = 3
+    var score = 0
+    var round = 1
     
     func points(sliderValue: Int) -> Int {
         // let diff = sliderValue == self.target ? 0 : abs(sliderValue - self.target);
         // return 100 - diff;
         100 - abs(sliderValue - target);
     }
+    
+    mutating func startNewRound(points: Int) {
+        score = score + points
+        round = round + 1
+    }
+    
+
 }
